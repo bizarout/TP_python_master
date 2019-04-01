04dadd9d9934ae8ec8b285563346b379ac2b1a35
bizarout <hamid.bezzout.tech@gmail.com>
2019-04-01 22:39:44 +0200

    ajout de l' exerice 2

---
+++ TD_1_exercice_2.py
@@ -0,0 +1,25 @@
+""" Exercice N: 2 
+    Auteur : H.BEZZOUT 
+"""    
+
+"""Déclaration des variables"""
+
+duree    = 8000   # déclaration du variable contenant le nombre de secondes 
+heures    = 0      # initialisation des trois variables , heures , minutes , secondes 
+minutes  = 0
+secondes = 0
+
+""" programme principale """ 
+
+print(" Entrer une durée : ")
+duree    = float(input( " la durée "))
+heures   = duree // 3600                # pour utiliser la division entiere il faut ustiliser l'opérateur : // 
+duree    = duree % 3600 
+minutes  = duree // 60 
+secondes = duree % 60
+
+print(str(heures)+" heure  "+str(minutes)+" minute  "+str(secondes)+" seconde")
+
+
+
+ 
