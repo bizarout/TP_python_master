9a5c0491cebf6e4185aa093fd5006626f0600597
bizarout <hamid.bezzout.tech@gmail.com>
2019-04-01 22:08:40 +0200

    ajout de la première exerice

---
+++ TD_1_exercice_1.py
@@ -0,0 +1,14 @@
+""" Exercice N: 1 
+    Auteur : H.BEZZOUT 
+    
+""" 
+
+
+pi = 3.14  # cr�ation de la constante pi 
+
+rayon = input(" Entrer le rayon du circle ") # demmande � l'utilisateur d'entrer le rayon 
+
+surfaceCircle = (float(rayon)**2)*pi  # l'instruction float permet de convertir une chaine de caractère en type float 
+
+print(" la surface du circle de rayon :",surfaceCircle) # le resulat afficher avec la fonction print 
+
